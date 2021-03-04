7e0919c37e0bc5efb9306c18f36a446709245ae3
xarvatium <ctfore2003@gmail.com>
2021-03-03 21:30:49 -0500

    Changed a var to a val when var wasn't needed.

--- src/main/kotlin/additionCalc.kt
+++ src/main/kotlin/additionCalc.kt
@@ -6,6 +6,6 @@ fun main() {
     val num1:Int = reader.nextInt()
     print("What is your second number? ")
     val num2:Int = reader.nextInt()
-    var sum = num1 + num2
+    val sum = num1 + num2
     print("The sum of $num1 and $num2 is: $sum")
 }
